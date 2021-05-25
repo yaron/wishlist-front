a741f60640060fdfd4029ad168467f47e69326de
Yaron Tal <yarontal12@gmail.com>
2021-05-25 22:36:34 +0200

    Add link to display

--- src/Item.svelte
+++ src/Item.svelte
@@ -11,11 +11,15 @@
 	<div class="title">
 		<h3>{item.name}</h3>
 	</div>
-	<div class="price">
-		<span>&euro;{(item.price / 100).toFixed(2)}</span>
-	</div>
 	<div class="image">
 		<img class="thumb" src={item.image} alt="" />
 	</div>
+	<div class="link">
+		<span><a target="_blank" href="{item.url}">Link</a></span>
+	</div>
+	<div class="price">
+		<span>&euro;{(item.price / 100).toFixed(2)}</span>
+	</div>
+	
 	<slot></slot>
 </section>
