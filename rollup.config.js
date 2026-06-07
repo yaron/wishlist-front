ca103dc27211763dc239cc60ee14219e58b5e54f
Yaron Tal <yaron@skyworkz.nl>
2026-06-07 22:52:05 +0200

    Fix dev server

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { spawn } from 'child_process';
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
@@ -17,7 +18,7 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+			server = spawn('npm', ['run', 'start', '--', '--dev'], {
 				stdio: ['ignore', 'inherit', 'inherit'],
 				shell: true
 			});
